e6ca99ce3fe813915f130d4cc0d8edb63efd5131
noventon <noventon@mamiteam.com>
2020-09-27 18:11:05 +0700

    feature/FE-001 Add Landing Page

---
+++ src/components/Nav.svelte
@@ -0,0 +1,67 @@
+<script>
+	export let segment;
+</script>
+
+<style>
+	nav {
+		font-weight: 300;
+		padding: 0 1em;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	/* clearfix */
+	ul::after {
+		content: '';
+		display: block;
+		clear: both;
+	}
+
+	li {
+		display: block;
+		float: left;
+	}
+
+	[aria-current] {
+		position: relative;
+		display: inline-block;
+	}
+
+	[aria-current]::after {
+		position: absolute;
+		content: '';
+		width: calc(100% - 1em);
+		height: 2px;
+		background-color: #F1CD69;
+		display: block;
+		bottom: -1px;
+	}
+
+	a {
+		text-decoration: none;
+		padding: 1em 0.5em;
+		display: block;
+	}
+
+	img {
+		margin: 10px 20px 0 20px; 
+	}
+</style>
+
+<nav class="flex justify-between">
+	<ul>
+		<li><img src="logo.png" alt="ULTRAG"></li>
+		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a></li>
+		<li><a aria-current="{segment === 'contest' ? 'page' : undefined}" href="contest">Contest</a></li>
+
+		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+		     the blog data when we hover over the link or tap it on a touchscreen -->
+		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
+	</ul>
+	<ul>
+		<li><a href="profile">Yosep Novento Nugroho</a></li>
+	</ul>
+</nav>
---
+++ src/routes/index.svelte
@@ -0,0 +1,23 @@
+<style>
+	span {
+		color: #F1CD69;
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<div class="flex justify-between pt-16 w-auto">	
+	<div class="pr-4">
+		<p class="text-5xl font-bold">Welcome to <span>ULTRAG</span></p>
+		<p class="py-2 max-w-xl text-2xl">ULTRAG is an online automated judge for coding
+			problems hosted by Computing Laboratory,
+			Telkom University.</p>
+		<p class="my-4 text-2xl">Improve your coding problem-solving skills here!</p>
+		<a href="contest" class="text-2xl bg-white hover:bg-gray-200 text-gray-800 font-semibold py-2 px-6 border border-yellow-500 rounded shadow">Solve a problem</a>
+	</div>
+	<div class="p-4">
+		<img src="manwithlaptop.png" height="326px" alt="Man With Laptop">
+	</div>
+</div>
